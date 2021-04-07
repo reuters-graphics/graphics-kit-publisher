54a8d8813c16e60500b3cb37c5527c67a79c70f3
hobbes7878 <jon.r.mcclure@gmail.com>
2021-04-07 09:21:37 +0100

    closes #3

--- src/constants/pack.js
+++ src/constants/pack.js
@@ -1,4 +1,4 @@
 export const DEFAULT_PACK_LOCALE = 'en';
 export const DEFAULT_PACK_METADATA_FILE = 'content.json';
-export const DEFAULT_PACK_TITLE_PROP = 'seoTitle';
-export const DEFAULT_PACK_DESCRIPTION_PROP = 'seoDescription';
+export const DEFAULT_PACK_TITLE_PROP = 'SeoTitle';
+export const DEFAULT_PACK_DESCRIPTION_PROP = 'SeoDescription';
