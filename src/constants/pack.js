cbaa65df3b9414c9e5d4e899f23e178788774ed8
hobbes7878 <jon.r.mcclure@gmail.com>
2021-04-16 14:07:30 +0100

    change metadata prop defaults

--- src/constants/pack.js
+++ src/constants/pack.js
@@ -1,4 +1,4 @@
 export const DEFAULT_PACK_LOCALE = 'en';
 export const DEFAULT_PACK_METADATA_FILE = 'content.json';
-export const DEFAULT_PACK_TITLE_PROP = 'SeoTitle';
-export const DEFAULT_PACK_DESCRIPTION_PROP = 'SeoDescription';
+export const DEFAULT_PACK_TITLE_PROP = 'SEOTitle';
+export const DEFAULT_PACK_DESCRIPTION_PROP = 'SEODescription';
