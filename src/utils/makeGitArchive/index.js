8902aecb420178cbfbe5326e6ee04c5b93cddf5a
hobbes7878 <jon.r.mcclure@gmail.com>
2021-04-26 13:11:46 +0100

    wrapping commit in a try block as original fix to #6 still tripping

--- src/utils/makeGitArchive/index.js
+++ src/utils/makeGitArchive/index.js
@@ -6,7 +6,12 @@ const ROOT = process.cwd();
 const git = simpleGit(ROOT);
 
 export default async() => {
-  await git.add('.');
-  await git.commit('Pre-archive', { '--allow-empty': null });
+  try {
+    await git.add('.');
+    await git.commit('Pre-archive', { '--allow-empty': null });
+  } catch (e) {
+    console.log('Could not git commit changes. Maybe nothing changed?');
+  }
+
   spawnSync('git', ['archive', '-o', 'packages/app.zip', 'HEAD', '.', ':!project-files'], { cwd: ROOT });
 };
