41afb8e8dfded64d23d888a15d8369ba14694c0a
hobbes7878 <jon.r.mcclure@gmail.com>
2021-05-06 10:28:50 +0100

    updates embed code

--- src/constants/embedCodes.js
+++ src/constants/embedCodes.js
@@ -1,3 +1,3 @@
 export const pymCodeFromTemplate = (embedSlug, embedUrl) => {
-  return `<div id="${embedSlug}"></div><script type="text/javascript">new pym.Parent("${embedSlug}", "${embedUrl}", {});</script>`;
+  return `<div id="${embedSlug}"></div><script type="text/javascript" src="//graphics.thomsonreuters.com/pym.min.js"></script><script type="text/javascript">new pym.Parent("${embedSlug}", "${embedUrl}", {});</script>`;
 };
