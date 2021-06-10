82dbc8aacbcf8f62623ab746d2e5523cefe16e61
hobbes7878 <jon.r.mcclure@gmail.com>
2021-06-11 00:23:13 +0100

    closes #11

--- src/preview/build/index.js
+++ src/preview/build/index.js
@@ -1,6 +1,8 @@
 import { PackageConfigError } from '../../exceptions/errors';
 import chalk from 'chalk';
+import fs from 'fs-extra';
 import getPkg from '../../utils/getPkg';
+import rimraf from 'rimraf';
 import { spawnSync } from 'child_process';
 
 export default {
@@ -11,6 +13,8 @@ export default {
   buildPreview() {
     const pkg = getPkg();
     if (!pkg.scripts['build:preview']) throw new PackageConfigError(chalk`{cyan package.json} has no {yellow build:preview} script. It needs one.`);
+    if (fs.existsSync(this.DIST_DIR)) rimraf.sync(this.DIST_DIR);
+    fs.mkdirpSync(this.DIST_DIR);
     spawnSync('npm', ['run', 'build:preview'], { stdio: ['pipe', process.stdout, process.stderr], cwd: this.CWD });
   },
 };
