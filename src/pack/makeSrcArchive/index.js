dcffcf98c94011f28fe8e0ff2964a1aa691d0274
hobbes7878 <jon.r.mcclure@gmail.com>
2021-08-27 19:25:03 +0100

    closes #19

--- src/pack/makeSrcArchive/index.js
+++ src/pack/makeSrcArchive/index.js
@@ -7,8 +7,12 @@ export default {
   async makeSrcArchive() {
     const PKG_ROOT = getPkgRoot();
     const git = simpleGit(PKG_ROOT);
-    await git.add('.');
-    await git.commit('Build for publish');
+    try {
+      await git.add('.');
+      await git.commit('Build for publish', { '--allow-empty': null });
+    } catch (e) {
+      console.log('Could not git commit changes. Maybe nothing changed?');
+    }
     const ARCHIVE_PATH = path.relative(PKG_ROOT, path.join(this.PACK_DIR, 'app.zip'));
     const ASSETS_DIR = path.relative(PKG_ROOT, this.ASSETS_DIR);
     spawnSync('git', ['archive', '-o', ARCHIVE_PATH, 'HEAD', '.', `:!${ASSETS_DIR}`, ':!project-files'], { cwd: PKG_ROOT });
