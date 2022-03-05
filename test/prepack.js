a111f6d431f85088fa6cbed5bdae934c657fef9c
hobbes7878 <jon.r.mcclure@gmail.com>
2022-03-05 13:42:06 +0000

    some prepack tests for metadata

--- test/prepack.js
+++ test/prepack.js
@@ -3,6 +3,10 @@ const expect = require('expect.js');
 const mock = require('mock-fs');
 const fs = require('fs');
 const path = require('path');
+const sinon = require('sinon');
+const prompts = require('prompts');
+
+let PKG;
 
 describe('GraphicsKitPublisher prepacks project', function() {
   this.timeout(20000);
@@ -19,10 +23,31 @@ describe('GraphicsKitPublisher prepacks project', function() {
         embeds: { en: { chart: { 'index.html': '<html></html>' } } },
       },
     }, { createCwd: false });
+    PKG = {
+      reuters: {
+        contact: {
+          name: 'Jon',
+          email: 'j@gmail.com',
+        },
+        graphic: {
+          desk: 'london',
+          slugs: {
+            root: 'HEALTH-CORONAVIRUS',
+            wild: '',
+          },
+          authors: [
+            { name: 'Jon', link: 'https://jm.co' },
+          ],
+          published: new Date().toISOString(),
+        },
+      },
+      homepage: 'https://www.google.com',
+    };
   });
 
   afterEach(function() {
     mock.restore();
+    sinon.restore();
   });
 
   it('Should set updated date in serverless', async function() {
@@ -44,6 +69,10 @@ describe('GraphicsKitPublisher prepacks project', function() {
     const pkg = JSON.parse(fs.readFileSync('package.json'));
     expect(pkg.reuters.graphic.updated).not.to.be(undefined);
     expect(new Date(pkg.reuters.graphic.updated)).to.be.an(Date);
+    expect(new Date(pkg.reuters.graphic.updated)).to.be.greaterThan(pubDate);
+    delete process.env.GRAPHICS_SERVER_USERNAME;
+    delete process.env.GRAPHICS_SERVER_PASSWORD;
+    delete process.env.GRAPHICS_SERVER_API_KEY;
   });
 
   it('Should not set updated date in serverless if homepage not set', async function() {
@@ -61,6 +90,9 @@ describe('GraphicsKitPublisher prepacks project', function() {
     await graphicsPublisher.setUpdatedTime();
     const pkg = JSON.parse(fs.readFileSync('package.json'));
     expect(pkg.reuters.graphic.updated).to.be(undefined);
+    delete process.env.GRAPHICS_SERVER_USERNAME;
+    delete process.env.GRAPHICS_SERVER_PASSWORD;
+    delete process.env.GRAPHICS_SERVER_API_KEY;
   });
 
   it('Should not set updated date in serverless if published date is in the future', async function() {
@@ -81,5 +113,84 @@ describe('GraphicsKitPublisher prepacks project', function() {
     await graphicsPublisher.setUpdatedTime();
     const pkg = JSON.parse(fs.readFileSync('package.json'));
     expect(pkg.reuters.graphic.updated).to.be(undefined);
+    delete process.env.GRAPHICS_SERVER_USERNAME;
+    delete process.env.GRAPHICS_SERVER_PASSWORD;
+    delete process.env.GRAPHICS_SERVER_API_KEY;
+  });
+
+  it('Should ask to set updated date', async function() {
+    const pubDate = new Date();
+    pubDate.setDate(pubDate.getDate() - 1);
+    fs.writeFileSync('package.json', JSON.stringify({
+      reuters: {
+        graphic: {
+          published: pubDate.toISOString(),
+        },
+      },
+      homepage: 'https://www.google.com',
+    }));
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ confirm: true }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.setUpdatedTime();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.graphic.updated).not.to.be(undefined);
+    expect(new Date(pkg.reuters.graphic.updated)).to.be.an(Date);
+  });
+
+  it('Should ask for missing contact name', async function() {
+    delete PKG.reuters.contact.name;
+    fs.writeFileSync('package.json', JSON.stringify(PKG));
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ 'reuters.contact.name': 'Jon' }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.getPackageMetadata();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.contact.name).to.be('Jon');
+  });
+
+  it('Should ask for missing contact email', async function() {
+    delete PKG.reuters.contact.email;
+    fs.writeFileSync('package.json', JSON.stringify(PKG));
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ 'reuters.contact.email': 'jon@gmail.com' }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.getPackageMetadata();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.contact.email).to.be('jon@gmail.com');
+  });
+
+  it('Should ask for missing desk', async function() {
+    delete PKG.reuters.graphic.desk;
+    fs.writeFileSync('package.json', JSON.stringify(PKG));
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ 'reuters.graphic.desk': 'london' }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.getPackageMetadata();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.graphic.desk).to.be('london');
+  });
+
+  it('Should ask for missing root slug', async function() {
+    delete PKG.reuters.graphic.slugs.root;
+    fs.writeFileSync('package.json', JSON.stringify(PKG));
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ 'reuters.graphic.slugs.root': 'HEALTH-CORONAVIRUS' }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.getPackageMetadata();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.graphic.slugs.root).to.be('HEALTH-CORONAVIRUS');
+  });
+
+  it('Should ask for publish date', async function() {
+    delete PKG.reuters.graphic.published;
+    fs.writeFileSync('package.json', JSON.stringify(PKG));
+    const now = new Date().toISOString();
+    const graphicsPublisher = new GraphicsPublisher();
+    const fake = sinon.fake.returns(Promise.resolve({ 'reuters.graphic.published': now }));
+    sinon.replace(prompts, 'prompt', fake);
+    await graphicsPublisher.getPackageMetadata();
+    const pkg = JSON.parse(fs.readFileSync('package.json'));
+    expect(pkg.reuters.graphic.published).to.be(now);
   });
 });
