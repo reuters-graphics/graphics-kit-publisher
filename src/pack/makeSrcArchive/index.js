c0a44247d4353548ec1b46b5bdd8ec04916d57ad
Jon McClure <jon.r.mcclure@gmail.com>
2023-08-18 13:06:17 +0100

    Build for publish

--- src/pack/makeSrcArchive/index.js
+++ src/pack/makeSrcArchive/index.js
@@ -1,7 +1,10 @@
+import fs from 'fs-extra';
 import getPkgRoot from '../../utils/getPkgRoot';
 import path from 'path';
 import simpleGit from 'simple-git/promise';
 import { spawnSync } from 'child_process';
+import unzipper from 'unzipper';
+import zipDir from '../../utils/zipDir';
 
 export default {
   async makeSrcArchive() {
@@ -13,8 +16,24 @@ export default {
     } catch (e) {
       console.log('Could not git commit changes. Maybe nothing changed?');
     }
-    const ARCHIVE_PATH = path.relative(PKG_ROOT, path.join(this.PACK_DIR, 'app.zip'));
+    const ARCHIVE_TEMP_PATH = path.join(this.PACK_DIR, '_app.zip');
+    const ARCHIVE_TEMP_RELATIVE_PATH = path.relative(PKG_ROOT, ARCHIVE_TEMP_PATH);
+    const ARCHIVE_DIR_PATH = path.join(this.PACK_DIR, 'app');
     const ASSETS_DIR = path.relative(PKG_ROOT, this.ASSETS_DIR);
-    spawnSync('git', ['archive', '-o', ARCHIVE_PATH, 'HEAD', '.', `:!${ASSETS_DIR}`, ':!project-files'], { cwd: PKG_ROOT });
+    spawnSync('git', ['archive', '-o', ARCHIVE_TEMP_RELATIVE_PATH, 'HEAD', '.', `:!${ASSETS_DIR}`, ':!project-files'], { cwd: PKG_ROOT });
+
+    // Re-zip the archive for:
+    // https://github.com/reuters-graphics/graphics-kit-publisher/issues/52
+    await new Promise((resolve) => {
+      fs.createReadStream(ARCHIVE_TEMP_PATH)
+        .pipe(
+          unzipper.Extract({ path: ARCHIVE_DIR_PATH })
+            .on('close', async() => {
+              fs.rmSync(ARCHIVE_TEMP_PATH);
+              await zipDir(ARCHIVE_DIR_PATH);
+              resolve();
+            })
+        );
+    });
   },
 };
---
+++ test/archive.js
@@ -0,0 +1,44 @@
+const GraphicsPublisher = require('../dist');
+const expect = require('expect.js');
+const mock = require('mock-fs');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+describe('GraphicsKitPublisher archives src files', function() {
+  this.timeout(30000);
+
+  beforeEach(function() {
+    mock({
+      [path.join(os.homedir(), '.reuters-graphics/profile.json')]: JSON.stringify({
+        name: 'Graphics Staff',
+        email: 'all.graphics@thomsonreuters.com',
+        url: 'https://www.reuters.com',
+        desk: 'london',
+      }),
+      'locales/en/content.json': JSON.stringify({ SEOTitle: 'title', SEODescription: 'description' }),
+      node_modules: mock.load(path.resolve(__dirname, '../node_modules')),
+      'src/statics/images/share.jpg': mock.load(path.resolve(__dirname, 'img.jpg')),
+      'media-assets': {
+        en: {
+          chart: {
+            'chart.jpg': mock.load(path.resolve(__dirname, 'img.jpg')),
+            'chart.eps': 'zzz',
+          },
+        },
+      },
+      'package.json': JSON.stringify({ scripts: { build: '' }, homepage: 'https://www.reuters.com/graphics/project/' }),
+      'graphics-pack': { '_app.zip': mock.load(path.resolve(__dirname, '_app.zip')) },
+    }, { createCwd: false });
+  });
+
+  afterEach(function() {
+    mock.restore();
+  });
+
+  it('Should create src archive', async function() {
+    const graphicsPublisher = new GraphicsPublisher();
+    await graphicsPublisher.makeSrcArchive(true);
+    expect(fs.existsSync('graphics-pack/app.zip')).to.be(true);
+  });
+});
