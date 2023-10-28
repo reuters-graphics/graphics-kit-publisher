2a7debc739296c95595c8d8b8d374528e0db79cf
Jon McClure <jon.r.mcclure@gmail.com>
2023-10-28 21:11:52 +0100

    updates for new client

--- src/publish/index.ts
+++ src/publish/index.ts
@@ -2,7 +2,9 @@ import { Elements, TeamsKlaxon } from '@reuters-graphics/teams-klaxon';
 
 import type { ConfigType } from '../setConfig';
 import type { PromptObject } from 'prompts';
+import { Publishing } from '@reuters-graphics/server-client';
 import chalk from 'chalk';
+import { flatten } from 'lodash-es';
 import getEmbedEditionSlugs from '../utils/getEmbedEditionSlugs';
 import getPackMetadata from '../prepack/getPackMetadata';
 import getPkg from '../utils/getPkg';
@@ -22,7 +24,7 @@ export default async (config: ConfigType) => {
   /**
    * Referrals are simply denoted with the last part of their
    * page path named "-referral". For example, a page like
-   * "embeds/en/referral/index.html" turns into "media-en-referral".
+   * "embeds/en/referral/index.html" turns into a "media-en-referral" slug.
    */
   const referralEmbedArchives = embedEditionSlugs
     .filter((slug) => /-referral$/.test(slug))
@@ -34,58 +36,51 @@ export default async (config: ConfigType) => {
     .filter((slug) => !/-referral$/.test(slug))
     .map((slug) => `${slug}.zip`);
   /**
-   * Public archives go outside the building, either
-   * as the public pages or through Connect.
+   * Public archives go outside the building, either as
+   * public pages or through Reuters Connect. Excludes referrals.
    */
   const publicArchives = [
-    'public.zip', // Public archive always exists.
+    'public.zip', // Public page archive always exists.
     ...clientEmbedArchives,
   ];
+  /**
+   * Embeddable archives can be embedded in dotcom articles
+   * through Lynx. Excludes public page archive.
+   */
+  const embeddableArchives = [...clientEmbedArchives, ...referralEmbedArchives];
+
+  /**
+   * All possible media editions, used as filters in the server client.
+   * (Not all of these actually will exist...)
+   */
+  const archivesToMedia: Publishing.PublishToMedia = flatten(
+    publicArchives.map((archive) => [
+      [archive, 'media-interactive'] as [string, string],
+      [archive, 'EPS'] as [string, string],
+    ])
+  );
+
+  /**
+   * All possible Lynx editions, used as filters in the server client.
+   * (Not all of these actually will exist...)
+   */
+  const archivesToLynx: Publishing.PublishToLynx = flatten(
+    embeddableArchives.map((archive) => [
+      [archive, 'interactive'] as [string, string],
+      [archive, 'JPG'] as [string, string],
+    ])
+  );
 
   let serverClient;
+  let revisionType: Publishing.PublishRevisionType = 'Refresh';
 
   if (process.env.GRAPHICS_SERVER_PUBLISH) {
     const MEDIA = process.env.GRAPHICS_SERVER_PUBLISH_TO_MEDIA
-      ? ['media-interactive', 'EPS']
+      ? archivesToMedia
       : false;
     const LYNX = process.env.GRAPHICS_SERVER_PUBLISH_TO_LYNX
-      ? ['interactive', 'JPG']
+      ? archivesToLynx
       : false;
-
-    const packMetadata = await getPackMetadata(config);
-
-    serverClient = await updateGraphicPack(packMetadata, config);
-
-    await serverClient.publishGraphic(publicArchives, MEDIA, LYNX, false);
-  } else {
-    const questions = [
-      {
-        type: 'confirm',
-        name: 'isCorrection',
-        message: 'Are you publishing a correction?',
-        initial: false,
-      },
-      {
-        type: 'confirm',
-        name: 'publishToLynx',
-        message:
-          'Should this graphic be published to Lynx (available to Reuters staff)?',
-        initial: false,
-      },
-      {
-        type: 'confirm',
-        name: 'publishToMedia',
-        message:
-          'Should this graphic be published to Connect (available to clients)?',
-        initial: false,
-      },
-    ] as PromptObject[];
-
-    const { isCorrection, publishToLynx, publishToMedia } =
-      await prompts(questions);
-
-    const MEDIA = publishToMedia ? ['media-interactive', 'EPS'] : false;
-    const LYNX = publishToLynx ? ['interactive', 'JPG'] : false;
 
     const packMetadata = await getPackMetadata(config);
 
@@ -95,19 +90,58 @@ export default async (config: ConfigType) => {
       publicArchives,
       MEDIA,
       LYNX,
-      isCorrection
+      revisionType
     );
-  }
+  } else {
+    const questions = [
+      {
+        type: 'select',
+        name: 'revision',
+        message: 'What type of update are you publishing?',
+        choices: [
+          {
+            title: 'Refresh - Updates code or fixes a superficial typo',
+            value: 'Refresh',
+          },
+          { title: 'Update - Adds new information', value: 'Update' },
+          { title: 'Correction - Corrects an error', value: 'Correction' },
+        ],
+        initial: 0,
+      },
+      {
+        type: 'confirm',
+        name: 'publishToLynx',
+        message:
+          'Should this graphic be promoted in Lynx (available to embed in dotcom articles)?',
+        initial: false,
+      },
+      {
+        type: 'confirm',
+        name: 'publishToMedia',
+        message:
+          'Should this graphic be published to Connect (available to media clients)?',
+        initial: false,
+      },
+    ] as PromptObject[];
 
-  if (referralEmbedArchives.length) {
-    // KILL THIS per https://github.com/reuters-graphics/graphics-kit-publisher/issues/62
-    // Any referrals are always published to Lynx and never to Connect.
-    // await serverClient.publishGraphic(
-    //   referralEmbedArchives,
-    //   false,
-    //   true,
-    //   false
-    // );
+    const { revision, publishToLynx, publishToMedia } =
+      await prompts(questions);
+
+    revisionType = revision as 'Refresh' | 'Update' | 'Correction';
+
+    const MEDIA = publishToMedia ? archivesToMedia : false;
+    const LYNX = publishToLynx ? archivesToLynx : false;
+
+    const packMetadata = await getPackMetadata(config);
+
+    serverClient = await updateGraphicPack(packMetadata, config);
+
+    await serverClient.publishGraphic(
+      publicArchives,
+      MEDIA,
+      LYNX,
+      revisionType
+    );
   }
 
   console.log(chalk`\n\nPublished to: {green ${pkg.homepage}}\n`);
