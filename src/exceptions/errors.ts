4a6bbe813865083e1edd98c632f6b860743d7c58
Jon McClure <jon.r.mcclure@gmail.com>
2024-06-21 14:07:00 +0100

    adds cli error handling. closes #73 closes #13

--- src/exceptions/errors.ts
+++ src/exceptions/errors.ts
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 export class LocationError extends Error {
   constructor(message: string) {
     super(message);
@@ -61,3 +63,28 @@ export class EditionArchiveError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+
+const coalesceToError = (err: unknown) => {
+  return err instanceof Error ||
+    (err && (err as any).name && (err as any).message)
+    ? (err as Error)
+    : new Error(JSON.stringify(err));
+};
+
+/**
+ * Log errors and exit the current process
+ * @param error
+ */
+export const handleError = (e: unknown) => {
+  const error = coalesceToError(e);
+  const prefix = chalk.bold.red('> Publisher ERROR:');
+  if (error.message) {
+    console.error(`${prefix} ${error.message}\n`);
+    if (error.stack) {
+      console.error(chalk.gray(error.stack.split('\n').slice(1).join('\n')));
+    }
+  } else {
+    console.error(`${prefix} ${error}`);
+  }
+  process.exit(1);
+};
