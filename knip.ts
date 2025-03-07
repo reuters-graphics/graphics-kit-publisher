0fcd899a4d40d0d52a86d00f202ea65a5f3e18bd
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-03-07 13:48:00 +0000

    remove temporary exceptions for knip

--- knip.ts
+++ knip.ts
@@ -3,8 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   entry: ['src/index.ts', 'src/cli.ts'],
   project: ['src/**/*.ts'],
-  // Temporary
-  ignore: ['src/config/package.ts', 'src/prompts/multiselect.ts'],
 };
 
 export default config;
